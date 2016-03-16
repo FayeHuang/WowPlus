83a7fa73dfdf92f1a1acef482b806e77eec3f07b
Faye <faye@cht.com.tw>
2016-03-16 15:00:19 +0800

    init

---
+++ assets/src/MemoryPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PageHead from './PageHead'
+
+export default class MemoryPage extends React.Component {
+	static propTypes = {};
+
+    static defaultProps = {};
+    
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+    	return (
+            <div className="page">
+    		    <PageHead   title="Memory Usage"
+                            dateTime="2016/03/03 10:23"
+                />      
+            </div>
+    	)
+    }
+}
---
+++ assets/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory, IndexRoute, IndexRedirect } from 'react-router'
+
+import Main from './Main'
+import DiskPage from './DiskPage'
+import CpuPage from './CpuPage'
+import MemoryPage from './MemoryPage'
+
+ReactDOM.render((
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRedirect to="/disk" />
+			<Route path="disk" component={DiskPage} />
+			<Route path="cpu" component={CpuPage} />
+			<Route path="memory" component={MemoryPage} />
+		</Route>
+	</Router>
+), document.getElementById('react-app'))
